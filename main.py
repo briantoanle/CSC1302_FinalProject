ab6be1d30fc832e4c906197ff964ea7d57e6a3ce
Toan Le <briantoanle@gmail.com>
2023-04-09 18:59:10 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+
+# raw collection of your choice (300 data points and 5 features aka 300 rows and 3 columns)
+# source: https://archive.ics.uci.edu/ml/machine-learning-databases/00616/
+
+#2.1 read in the data
+# we are only using the first 1000 lines of data
+tetuan = pd.read_csv('Tetuan City power consumption.csv',nrows = 5000)
+#2.2 clean data, drop empty rows
+tetuan = tetuan.dropna()
+
+print(tetuan)
