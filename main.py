f178389d7096983eba9582a057c95676ee811a87
Toan Le <briantoanle@gmail.com>
2023-04-20 18:35:36 -0400

    Added temperature-date graph

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ import matplotlib as mpl
 # raw collection of your choice (300 data points and 5 features aka 300 rows and 3 columns)
 # source: https://archive.ics.uci.edu/ml/machine-learning-databases/00616/
 
-#2.1 read in the data
+# 2.1 read in the data
 # we are only using the first 1000 lines of data
 tetuan = pd.read_csv('Tetuan City power consumption.csv',nrows = 5000)
-#2.2 clean data, drop empty rows
+# 2.2 clean data, drop empty rows
 tetuan = tetuan.dropna()
 
-print(tetuan)
